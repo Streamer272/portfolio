2b49fa0a609deef0562570986bcf8a8e243fffc8
Daniel Svitan <daniel.svitan.team7274dev@gmail.com>
2025-01-03 09:50:26 +0100

    :fire: Removes useless shit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,24 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    fontFamily: {
-      dm: ["'DM Sans'", "sans-serif"],
-      "dm-bold": ["'DM Sans Bold'", "sans-serif"],
-    },
-    colors: {
-      transparent: "transparent",
-      primary: "#2E323E",
-      onPrimary: "#FFFFFF",
-      secondary: "#565A66",
-      onSecondary: "#FAFAD2",
-      accent: "#81858F",
-      onAccent: "#FFE4C4",
-      background: "#17191E",
-      onBackground: "#FFFFFF",
-    },
     extend: {
       width: {
         page: "80vw",
+      },
+      fontFamily: {
+        dm: ["'DM Sans'", "sans-serif"],
+        "dm-bold": ["'DM Sans Bold'", "sans-serif"],
+      },
+      colors: {
+        s1: "#171717",
+        s2: "#424242",
+        s3: "#4a4a4a",
+        s4: "#b0bec5",
+        t1: "#e5e5e5",
+        t2: "#cccccc",
       },
     },
   },
